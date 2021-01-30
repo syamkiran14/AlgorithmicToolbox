2490b49f036c29420622cd4099d8dee3eff8b7f5
Syam kiran <27140577+syamkiran14@users.noreply.github.com>
2021-01-31 05:04:48 +0530

    Add files via upload

---
+++ AplusB.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+	using namespace std;
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    cin >> firstNumber >> secondNumber;
+    sumOfTwoNumbers = firstNumber + secondNumber; 
+    cout << sumOfTwoNumbers;     
+	return 0;
+}
